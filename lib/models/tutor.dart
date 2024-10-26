2a2d9c9db57f8d55c91eb72b523b824854aebd76
nicholas <nwks072003@gmail.com>
2024-10-27 03:25:55 +0800

    tuition testing

--- lib/models/tutor.dart
+++ lib/models/tutor.dart
@@ -1,13 +1,34 @@
+
 class Tutor {
+
   final String name;
+
+  final String subject; // Add this line
+
   final double rating;
+
+  final String description;
+
   final String imageUrl;
-  final String course; 
+
+  final String course;
+
+
 
   Tutor({
+
     required this.name,
+
+    required this.subject, // Add this line
+
     required this.rating,
+
+    required this.description,
+
     required this.imageUrl,
+
     required this.course,
+
   });
+
 }
