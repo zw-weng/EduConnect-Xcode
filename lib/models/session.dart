8fd143bee91a0b5c371fa60c2f2f99f0e78666e0
nicholas <nwks072003@gmail.com>
2024-10-28 02:23:12 +0800

    modified tutor screen got upcoming session and book tutor option

---
+++ lib/models/session.dart
@@ -0,0 +1,15 @@
+class Session {
+  final String tutorName;
+  final String subject;
+  final String startTime;
+  final String endTime;
+  final String date; 
+
+  Session({
+    required this.tutorName,
+    required this.subject,
+    required this.startTime,
+    required this.endTime,
+    required this.date, 
+  });
+}
